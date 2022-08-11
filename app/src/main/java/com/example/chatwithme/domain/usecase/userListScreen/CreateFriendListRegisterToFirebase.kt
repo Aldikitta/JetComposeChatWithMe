3f61329962dc19f7f4bd98d2919dcdcbcadb5a59
Aldik <andi.kitta77@gmail.com>
2022-08-11 15:34:05 +0800

    usecase

---
+++ app/src/main/java/com/example/chatwithme/domain/usecase/userListScreen/CreateFriendListRegisterToFirebase.kt
@@ -0,0 +1,19 @@
+package com.example.chatwithme.domain.usecase.userListScreen
+
+import com.example.chatwithme.domain.repository.UserListScreenRepository
+
+class CreateFriendListRegisterToFirebase(
+    private val userListScreenRepository: UserListScreenRepository
+) {
+    suspend operator fun invoke(
+        chatRoomUUID: String,
+        acceptorEmail: String,
+        acceptorUUID: String,
+        acceptorOneSignalUserId: String
+    ) = userListScreenRepository.createFriendListRegisterToFirebase(
+        chatRoomUUID,
+        acceptorEmail,
+        acceptorUUID,
+        acceptorOneSignalUserId
+    )
+}
