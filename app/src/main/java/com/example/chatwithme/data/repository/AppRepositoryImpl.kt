35fda6f8a8399676ed5e0fb6c007660eb17a7047
Aldik <andi.kitta77@gmail.com>
2022-08-11 22:06:33 +0800

    AppRepositoryImpl

--- app/src/main/java/com/example/chatwithme/data/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/example/chatwithme/data/repository/AppRepositoryImpl.kt
@@ -1,7 +1,63 @@
 package com.example.chatwithme.data.repository
 
+import com.example.chatwithme.core.Constants.ERROR_MESSAGE
+import com.example.chatwithme.domain.repository.AuthScreenRepository
+import com.example.chatwithme.utils.Response
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AppRepositoryImpl {
+class AppRepositoryImpl @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val storage: FirebaseStorage,
+    private val database: FirebaseDatabase
+) : AuthScreenRepository {
+    override fun isUserAuthenticatedInFirebase(): Flow<Response<Boolean>> = flow {
+        try {
+            emit(Response.Loading)
+            if (auth.currentUser != null) {
+                emit(Response.Success(true))
+            } else {
+                emit(Response.Success(false))
+            }
+        } catch (e: Exception) {
+            emit(Response.Success(false))
+        }
+    }
+
+    override suspend fun signIn(email: String, password: String): Flow<Response<Boolean>> =
+        callbackFlow {
+            try {
+                this@callbackFlow.trySendBlocking(Response.Loading)
+                auth.signInWithEmailAndPassword(email, password).apply {
+                    this.await()
+                    if (this.isSuccessful) {
+                        this@callbackFlow.trySendBlocking(Response.Success(true))
+                    } else {
+                        this@callbackFlow.trySendBlocking(Response.Success(false))
+                    }
+                }
+            } catch (e: Exception) {
+                this@callbackFlow.trySendBlocking(Response.Error(e.message ?: ERROR_MESSAGE))
+            }
+
+            awaitClose {
+                channel.close()
+                cancel()
+            }
+        }
+
+    override suspend fun signUp(email: String, password: String): Flow<Response<Boolean>> {
+        TODO("Not yet implemented")
+    }
 }
