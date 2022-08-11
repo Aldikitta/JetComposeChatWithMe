e5d975a65b046c8ea25e8aed81b0831a6a007666
Aldik <andi.kitta77@gmail.com>
2022-08-11 14:45:54 +0800

    repository

--- app/src/main/java/com/example/chatwithme/domain/repository/AppRepository.kt
+++ app/src/main/java/com/example/chatwithme/domain/repository/AppRepository.kt
@@ -1,5 +1,10 @@
 package com.example.chatwithme.domain.repository
 
+import android.net.Uri
+import com.example.chatwithme.domain.model.FriendListRegister
+import com.example.chatwithme.domain.model.FriendListRow
+import com.example.chatwithme.domain.model.User
+import com.example.chatwithme.domain.model.UserStatus
 import com.example.chatwithme.utils.Response
 import kotlinx.coroutines.flow.Flow
 
@@ -9,4 +14,20 @@ interface AppRepository {
     fun isUserAuthenticatedInFirebase(): Flow<Response<Boolean>>
     suspend fun signIn(email: String, password: String): Flow<Response<Boolean>>
     suspend fun signUp(email: String, password: String): Flow<Response<Boolean>>
+
+    //Profile Screen
+    suspend fun signOut(): Flow<Response<Boolean>>
+    suspend fun uploadPictureToFirebase(url: Uri): Flow<Response<String>>
+    suspend fun createOrUpdateProfileToFirebase(user: User): Flow<Response<Boolean>>
+    suspend fun loadProfileFromFirebase(): Flow<Response<User>>
+    suspend fun setUserStatusToFirebase(userStatus: UserStatus): Flow<Response<Boolean>>
+
+    //UserListScreen
+    suspend fun loadAcceptedFriendRequestListFromFirebase(): Flow<Response<List<FriendListRow>>>
+    suspend fun loadPendingFriendRequestListFromFirebase(): Flow<Response<List<FriendListRegister>>>
+
+    suspend fun searchUserFromFirebase(userEmail: String): Flow<Response<User?>>
+
+    suspend fun checkChatRoomExistedFromFirebase(acceptorUUID: String): Flow<Response<String>>
+    suspend fun createChatRoomToFirebase(acceptorUUID: String): Flow<Response<String>>
 }
