7c5633833c04e715569a4d4451b569ff29abf91d
Aldik <andi.kitta77@gmail.com>
2022-08-11 21:17:41 +0800

    usecase

---
+++ app/src/main/java/com/example/chatwithme/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.example.chatwithme.di
+
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.chatwithme.domain.repository.AuthScreenRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("login")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    fun provideFirebaseAuthInstance() = FirebaseAuth.getInstance()
+
+    @Provides
+    fun provideFirebaseStorageInstance() = FirebaseStorage.getInstance()
+
+    @Provides
+    fun provideFirebaseDatabaseInstance() = FirebaseDatabase.getInstance()
+
+//    @Provides
+//    fun provideSharedPreferences(application: Application) =
+//        application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
+    @Provides
+    fun providesDataStore(application: Application) = application.dataStore
+
+//    @Provides
+//    fun provideAuthRepository(
+//        auth: FirebaseAuth,
+//        storage: FirebaseStorage,
+//        database: FirebaseDatabase
+//    ): AuthScreenRepository =
+
+}
