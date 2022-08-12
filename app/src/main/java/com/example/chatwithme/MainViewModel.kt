ea74dd720cb7072a110bf320355b049428186248
Aldik <andi.kitta77@gmail.com>
2022-08-12 15:48:24 +0800

    MainViewModel

--- app/src/main/java/com/example/chatwithme/MainViewModel.kt
+++ app/src/main/java/com/example/chatwithme/MainViewModel.kt
@@ -4,17 +4,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.chatwithme.domain.model.UserStatus
 import com.example.chatwithme.domain.usecase.authScreen.AuthUseCases
+import com.example.chatwithme.domain.usecase.profileScreen.ProfileScreenUseCases
+import com.example.chatwithme.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val useCases: AuthUseCases
+    private val useCases: ProfileScreenUseCases
 ) : ViewModel() {
-    fun setUserStatusToFirebase(userStatus: UserStatus){
+    fun setUserStatusToFirebase(userStatus: UserStatus) {
         viewModelScope.launch {
-//            useCases.setu
+            useCases.setUserStatusToFirebase(userStatus).collect { response ->
+                when (response) {
+                    is Response.Loading -> {}
+                    is Response.Success -> {}
+                    is Response.Error -> {}
+                }
+            }
         }
     }
 }
