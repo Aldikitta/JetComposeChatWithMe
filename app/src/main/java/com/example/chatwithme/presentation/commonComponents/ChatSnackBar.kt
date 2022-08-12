1149b6b51ddf6096733586b0eabee75224153540
Aldik <andi.kitta77@gmail.com>
2022-08-12 16:16:40 +0800

    MainViewModel

--- app/src/main/java/com/example/chatwithme/presentation/commonComponents/ChatSnackBar.kt
+++ app/src/main/java/com/example/chatwithme/presentation/commonComponents/ChatSnackBar.kt
@@ -1,18 +1,11 @@
 package com.example.chatwithme.presentation.commonComponents
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ChatSnackBar(
     snackbarData: SnackbarData,
-    modifier: Modifier = Modifier,
     actionOnNewLine: Boolean = false,
 ) {
     val actionLabel = snackbarData.visuals.actionLabel
