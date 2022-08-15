2b073b851f5f19a29bd0dd857acb033d929a3c79
Aldik <andi.kitta77@gmail.com>
2022-08-15 15:23:37 +0800

    ProfileScreen

--- app/src/main/java/com/example/chatwithme/presentation/userlist/UserListViewModel.kt
+++ app/src/main/java/com/example/chatwithme/presentation/userlist/UserListViewModel.kt
@@ -2,10 +2,14 @@ package com.example.chatwithme.presentation.userlist
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.chatwithme.domain.model.FriendListRegister
 import com.example.chatwithme.domain.model.FriendListRow
 import com.example.chatwithme.domain.usecase.userListScreen.UserListScreenUseCases
+import com.example.chatwithme.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,4 +27,68 @@ class UserListViewModel @Inject constructor(
 
     var toastMessage = mutableStateOf("")
         private set
+
+    fun refreshingFriendList() {
+        viewModelScope.launch {
+            isRefreshing.value = true
+
+        }
+    }
+
+    fun createFriendshipRegisterToFirebase(acceptorEmail: String) {
+        //Search User -> Check Chat Room -> Create Chat Room -> Check FriendListRegister -> Create FriendListRegister
+        viewModelScope.launch {
+            userListScreenUseCases.searchUserFromFirebase.invoke(acceptorEmail)
+                .collect { response ->
+                    when (response) {
+                        is Response.Loading -> {
+                            toastMessage.value = ""
+                        }
+                        is Response.Success -> {
+                            if (response.data != null) {
+
+                            }
+                        }
+                        is Response.Error -> {}
+                    }
+
+                }
+        }
+    }
+
+    fun acceptPendingFriendRequestToFirebase(registerUUID: String) {
+        viewModelScope.launch {
+            userListScreenUseCases.acceptPendingFriendRequestToFirebase.invoke(registerUUID)
+                .collect { response ->
+                    when (response) {
+                        is Response.Loading -> {
+                            toastMessage.value = ""
+                        }
+                        is Response.Success -> {
+                            toastMessage.value = "Friend Request Accepted"
+                        }
+                        is Response.Error -> {}
+                    }
+                }
+        }
+    }
+
+    fun cancelPendingFriendRequestToFirebase(registerUUID: String) {
+        viewModelScope.launch {
+            userListScreenUseCases.rejectPendingFriendRequestToFirebase.invoke(registerUUID)
+                .collect { response ->
+                    when (response) {
+                        is Response.Loading -> {
+                            toastMessage.value = ""
+                        }
+                        is Response.Success -> {
+                            toastMessage.value = "Friend Request Canceled"
+                        }
+                        is Response.Error -> {}
+                    }
+                }
+        }
+    }
+
+
 }
