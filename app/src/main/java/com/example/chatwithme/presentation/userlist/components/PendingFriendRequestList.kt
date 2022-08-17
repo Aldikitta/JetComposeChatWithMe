c1cf4cda8b7dd913e5283f8c709229180472bd6b
Aldik <andi.kitta77@gmail.com>
2022-08-17 17:54:55 +0800

    CharScreen

--- app/src/main/java/com/example/chatwithme/presentation/userlist/components/PendingFriendRequestList.kt
+++ app/src/main/java/com/example/chatwithme/presentation/userlist/components/PendingFriendRequestList.kt
@@ -1,56 +1,50 @@
 package com.example.chatwithme.presentation.userlist.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
 import com.example.chatwithme.domain.model.FriendListRegister
+import com.example.chatwithme.ui.theme.spacing
 
 @Composable
 fun PendingFriendRequestList(
     item: FriendListRegister,
-    onAcceptClick:() -> Unit = {},
-    onCancelClick:() -> Unit = {}){
+    onAcceptClick: () -> Unit = {},
+    onCancelClick: () -> Unit = {}
+) {
     Card(
-        shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .fillMaxSize()
-            .padding(2.dp),
-    ){
-        Row(modifier = Modifier.height(60.dp), verticalAlignment = Alignment.CenterVertically) {
-            Box(modifier = Modifier) {
-                Text(text = item.requesterEmail,
-                    modifier = Modifier
-                        .padding(10.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    vertical = MaterialTheme.spacing.medium,
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier.padding(MaterialTheme.spacing.small),
+                text = item.requesterEmail,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+            )
+            Row() {
+                TextButton(
+                    onClick = { onCancelClick() }
+                ) {
+                    Text(text = "Decline", color = MaterialTheme.colorScheme.error)
+                }
+                TextButton(onClick = { onAcceptClick() }) {
+                    Text(text = "Accept")
+                }
             }
-            Icon(
-                imageVector = Icons.Filled.Done,
-                tint = Color.Green,
-                contentDescription = null,
-                modifier = Modifier
-                    .clickable {
-                        onAcceptClick()
-                    }
-                    .align(Alignment.CenterVertically))
-            Icon(
-                imageVector = Icons.Filled.Clear,
-                tint = Color.Red,
-                contentDescription = null,
-                modifier = Modifier
-                    .clickable {
-                        onCancelClick()
-                    }
-                    .align(Alignment.CenterVertically))
+
         }
     }
 }
